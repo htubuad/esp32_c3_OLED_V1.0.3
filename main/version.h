9a0002d478aaae215d830ca28de603747472e4ae
htubuad <846155179@qq.com>
2026-09-13 18:49:24 +0800

    chore: bump version to 1.3.6

--- main/version.h
+++ main/version.h
@@ -2,7 +2,7 @@
 #define APP_VERSION_H
 
 #define APP_NAME          "ESP32_C3"
-#define APP_VERSION       "1.3.5"
+#define APP_VERSION       "1.3.6"
 #define APP_BUILD_DATE    __DATE__ " " __TIME__
 #define APP_CHIP          "ESP32-C3"
 
