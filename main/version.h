ae1dd9ad3f7fbd0655777a93ec330feaf2c3e7a9
htubuad <846155179@qq.com>
2026-09-13 11:12:28 +0800

    fix: APP_VERSION V1.3.4 -> 1.3.4 匹配阿里云升级包版本号

--- main/version.h
+++ main/version.h
@@ -2,7 +2,7 @@
 #define APP_VERSION_H
 
 #define APP_NAME          "ESP32_C3"
-#define APP_VERSION       "V1.3.4"
+#define APP_VERSION       "1.3.4"
 #define APP_BUILD_DATE    __DATE__ " " __TIME__
 #define APP_CHIP          "ESP32-C3"
 
